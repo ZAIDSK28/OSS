3d7ab4d824a6d1534bb915425fb60919464afb33
ZaidShaikh <108930013+ZaddSK@users.noreply.github.com>
2022-07-08 15:46:55 +0530

    Add files via upload

---
+++ Pthread.java
@@ -0,0 +1,11 @@
+
+public class Pthread {
+
+	public static void main(String[] args) {
+		Thread t= Thread.currentThread();
+		System.out.println(t.getName());
+		System.out.println(t.getPriority());
+	}
+
+	
+}
